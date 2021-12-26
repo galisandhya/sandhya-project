eb24ad66b11f973fe89cfad9cd2f9c47fe7ff297
Galisandhya <galisandhya6@gmail.com>
2021-12-26 17:48:46 +0530

    this is muliplycation programme

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+void add(int x,int y){
+	int c = x + y;
+	std::cout << c << std::endl;
+}
+
+main(){
+	int a = 10;
+	int b = 20;
+	add(a,b);
+	
+}
---
+++ mul.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+void mul(int x, int y){
+	int z = x + y;
+	std::cout<<"z"<<z<<std::end;
+}
+int main(){
+	int a = 10 , b = 20;
+	mul(a , b);
+}
